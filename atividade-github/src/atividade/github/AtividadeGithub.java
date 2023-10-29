34266d7ea1760d9d3cd18bea003e2c2df23ff8e9
Antonio Mussato <antonio.muszato@gmail.com>
2023-10-29 16:08:11 -0400

    Primeiro exercicio

---
+++ atividade-github/src/atividade/github/AtividadeGithub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade.github;
+
+
+public class AtividadeGithub {
+
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello Git!");
+        
+
+        
+    }
+    
+}
